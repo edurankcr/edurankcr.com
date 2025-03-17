1c788fa412a60ffdb01cb8b38989b40cb01e81b9
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 22:19:32 -0600

    feat: add Section component for consistent layout and styling

---
+++ src/@theme/components/Section/Section.tsx
@@ -0,0 +1,20 @@
+import { Box } from '@theme/components';
+import type { ComponentProps, FC } from 'react';
+import { memo } from 'react';
+
+type SectionProps = ComponentProps<typeof Box>;
+
+const Section: FC<SectionProps> = memo(({
+  ...props
+}) => (
+  <Box
+    width="full"
+    paddingY="section"
+    as="section"
+    {...props}
+  />
+));
+
+Section.displayName = 'Section';
+
+export { Section };
