e9d85b60e21ff1f33c88d432ecd37a3aab5d15c4
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 22:19:52 -0600

    feat: rename providers.tsx to Providers.tsx and update message retrieval for locale support

--- src/@theme/providers/Providers.tsx
+++ src/@theme/providers/Providers.tsx
@@ -1,9 +1,10 @@
+import { getMessagesForLocale } from '@theme/functions';
 import type { IProviders } from '@theme/types';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
 
-const Providers = async ({ children }: IProviders) => {
-  const messages = await getMessages();
+const Providers = async ({ children, params }: IProviders) => {
+  const { locale } = await params;
+  const messages = await getMessagesForLocale({ locale });
 
   return (
     <NextIntlClientProvider messages={messages}>
