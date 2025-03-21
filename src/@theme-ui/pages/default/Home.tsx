200c1a124b57efbc083d8271dbf3bea625a71261
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-21 11:46:31 -0600

    feat: update Home component to include heading and review card layout

--- src/@theme-ui/pages/default/Home.tsx
+++ src/@theme-ui/pages/default/Home.tsx
@@ -1,18 +1,41 @@
-import { Section, Text } from '@theme/components';
+import { Box, Section, Stack } from '@theme/components';
 import type { IDictionary } from '@theme/types';
+import { ReviewCard } from '@theme/widgets';
+import { Heading } from '@theme-ui/components';
 import { HeroSection } from '@theme-ui/sections';
+import { useFormatter } from 'next-intl';
 
 type HomeProps = {} & IDictionary;
 
 export const Home = ({ dictionary }: HomeProps) => {
+  const format = useFormatter();
   return (
     <>
       <HeroSection dictionary={dictionary} />
-      <Section as="section" container>
-        <Text>
-          Hello World!
-          This is the home page.
-        </Text>
+      <Section asChild>
+        <Stack as="section" alignItems="center" container>
+          <Heading>
+            {dictionary('Heading.last_activity')}
+          </Heading>
+          <Box className="grid-reviews">
+            <ReviewCard
+              User={{
+                Name: 'Alejandro',
+                LastName: 'Marin',
+                UserName: 'btwxyz',
+              }}
+              Review={{
+                ReviewId: '123456789',
+                IsVerified: false,
+                Rating: 3.5,
+                ExperienceText: 'Hello world!',
+                CreatedAt: new Date('2023-10-01T12:00:00Z'),
+              }}
+              dictionary={dictionary}
+              formater={format}
+            />
+          </Box>
+        </Stack>
       </Section>
     </>
   );
