0eee5e191bcf342ef31ea77400d4c866e5d16d10
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-20 18:39:11 -0600

    feat: fix props spreading in CtaRanking component for proper link functionality

--- src/@theme-ui/components/CallToAction/CtaRanking.tsx
+++ src/@theme-ui/components/CallToAction/CtaRanking.tsx
@@ -6,8 +6,8 @@ type CtaRankingProps = {} & ComponentProps<typeof Link>;
 const CtaRanking = ({ ...props }: CtaRankingProps) => {
   return (
     <Link
-      {...props}
       text={{ size: '300-res', color: 'white', weight: 'bold', underline: true, align: 'center' }}
+      {...props}
     />
   );
 };
