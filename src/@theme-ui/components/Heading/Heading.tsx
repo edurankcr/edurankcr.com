ba169a268b2d1f4250d7d3a441fcca29b83d5fbe
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-20 19:19:32 -0600

    feat: add HeadingHero component for enhanced heading display

---
+++ src/@theme-ui/components/Heading/Heading.tsx
@@ -0,0 +1,20 @@
+import { Text } from '@theme/components';
+import type { IComponent } from '@theme/types';
+
+type HeadingProps = {} & IComponent;
+
+const HeadingHero = ({ children }: HeadingProps) => {
+  return (
+    <Text
+      as="h1"
+      color="white"
+      size="700-res"
+      weight="medium"
+      align="center"
+    >
+      {children}
+    </Text>
+  );
+};
+
+export { HeadingHero };
