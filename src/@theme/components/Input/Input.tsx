0647bf27503b1a2a84a446e45be007760b52442f
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-23 13:44:38 -0600

    feat: refactor Input component to simplify structure and enhance styling

--- src/@theme/components/Input/Input.tsx
+++ src/@theme/components/Input/Input.tsx
@@ -1,75 +1,45 @@
+import { Box } from '@theme/components';
 import type { VariantProps } from 'class-variance-authority';
 import { cva, cx } from 'class-variance-authority';
 import type { ComponentProps, ReactNode } from 'react';
 import { memo } from 'react';
 
-const inputVariants = cva('outline-0 w-full', {
-  variants: {
-    fontSize: {
-      300: 'text-300',
-    },
-    fontWeight: {
-      normal: 'font-normal',
-      medium: 'font-medium',
-      semibold: 'font-semibold',
-      bold: 'font-bold',
-    },
-    height: {
-      sm: 'h-9',
-      md: 'h-10',
-      lg: 'h-18',
-    },
-    paddingX: {
-      sm: 'px-4',
-      md: 'px-6',
-      lg: 'px-8',
-    },
-    shadow: {
-      200: 'shadow-200',
-    },
-    width: {
-      full: 'w-full',
-    },
-  },
-  defaultVariants: {
-    fontWeight: 'medium',
-    height: 'md',
-    paddingX: 'md',
-  },
+const inputVariants = cva('w-full outline-0 placeholder:text-neutral-600 text-ellipsis font-medium', {
+  variants: {},
+  defaultVariants: {},
 });
 
 type InputProps = {
   leftSection?: ReactNode;
   rightSection?: ReactNode;
+  boxSettings?: & ComponentProps<typeof Box>;
 } & ComponentProps<'input'> & VariantProps<typeof inputVariants>;
 
 const Input = memo<InputProps>(({
   className,
   leftSection,
   rightSection,
-  fontSize,
-  fontWeight,
-  height,
-  paddingX,
-  shadow,
-  width,
+  boxSettings,
   ...props
 }) => {
   return (
-    <div className={cx(inputVariants({
-      fontSize,
-      fontWeight,
-      height,
-      shadow,
-    }), 'inline-flex w-full items-center rounded-full bg-white')}
+    <Box
+      display="inlineFlex"
+      paddingX="md"
+      rounded="full"
+      width="full"
+      bgBackground="white"
+      justifyContent="center"
+      alignItems="center"
+      {...boxSettings}
     >
       {leftSection && <span className="me-4 text-neutral-600">{leftSection}</span>}
       <input
-        className={cx(className, 'w-full outline-0 placeholder:text-neutral-600 text-ellipsis')}
+        className={cx(inputVariants(), className)}
         {...props}
       />
       {rightSection && <span className="ms-4 text-neutral-600">{rightSection}</span>}
-    </div>
+    </Box>
   );
 });
 
