ef38699349af6be87b562c1bc3bf54fa8396eaab
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-28 10:06:41 -0600

    feat: refactor API module to use environment variable for base URL and simplify error handling

--- src/@theme/services/api.ts
+++ src/@theme/services/api.ts
@@ -1,29 +1,13 @@
 import axios from 'axios';
 
-export class ApiError extends Error {
-  type: string;
-  status: number;
-  traceId?: string | any;
-  errorCodes?: string[] | any;
+import { Env } from './env';
 
-  constructor(data: {
-    type: string;
-    title: string;
-    status: number;
-    traceId?: string;
-    errorCodes?: string[];
-  }) {
-    super(data.title);
-    this.name = 'ApiError';
-    this.type = data.type;
-    this.status = data.status;
-    this.traceId = data.traceId;
-    this.errorCodes = data.errorCodes;
-  }
-}
+const api = axios.create({
+  baseURL: Env.NEXT_PUBLIC_API_URL,
+});
 
 async function postAuthentication(Identifier: string, Password: string) {
-  return await axios.post('https://localhost:7242/auth/login', {
+  return await api.post('/auth/login', {
     Identifier,
     Password,
   });
