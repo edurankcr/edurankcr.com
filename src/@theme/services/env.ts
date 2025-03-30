87bede01be70e169a95c36c53f95f067c711125e
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-29 19:51:48 -0600

    feat(env): add JWT_SECRET and NEXT_PUBLIC_JWT_COOKIE_NAME to environment configuration

--- src/@theme/services/env.ts
+++ src/@theme/services/env.ts
@@ -2,10 +2,13 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const Env = createEnv({
-  server: {},
+  server: {
+    JWT_SECRET: z.string().min(1),
+  },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().optional(),
     NEXT_PUBLIC_API_URL: z.string().min(1),
+    NEXT_PUBLIC_JWT_COOKIE_NAME: z.string().min(1),
   },
   shared: {
     NODE_ENV: z.enum(['test', 'development', 'production']).optional(),
@@ -13,6 +16,8 @@ export const Env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_JWT_COOKIE_NAME: process.env.NEXT_PUBLIC_JWT_COOKIE_NAME,
+    JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
   },
 });
