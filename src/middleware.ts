f2c187f2dde7be75ba58a68f2e72add82aa89da9
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-31 22:38:17 -0600

    feat(middleware): implement token verification and redirect for guest routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,38 @@
+import { deleteTokenCookie, getTokenFromCookie, verifyToken } from '@theme/services';
 import { routing } from '@theme/services/routing';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware(routing);
+import { Routes } from '@/routes';
+
+const intlMiddleware = createMiddleware(routing);
+
+const guestRoutes = [
+  Routes.Guest.Login,
+  Routes.Guest.Register,
+  Routes.Guest.Email.Request,
+  Routes.Guest.Email.Sent,
+  Routes.Guest.Email.Verify,
+];
+
+export async function middleware(request: NextRequest) {
+  const response = intlMiddleware(request);
+  const token = await getTokenFromCookie();
+  const pathname = request.nextUrl.pathname;
+  const isGuestRoute = guestRoutes.some(route => pathname.includes(route));
+
+  if (token && isGuestRoute) {
+    const valid = await verifyToken(token);
+    if (valid) {
+      return NextResponse.redirect(new URL(Routes.Global.Home, request.url));
+    } else {
+      await deleteTokenCookie();
+    }
+  }
+
+  return response;
+}
 
 export const config = {
   matcher: [
