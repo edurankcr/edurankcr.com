0c1e8fad24557789097ca74bebd68678ad8597f0
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-05 17:13:47 -0600

    feat: add Setting component for page layout with header, footer, and loading screen

---
+++ src/ui/layouts/Setting/Setting.tsx
@@ -0,0 +1,27 @@
+import { getTranslations } from 'next-intl/server';
+
+import { Stack } from '@/components';
+import type { IPageLayout } from '@/types';
+
+import { Footer } from '../Footer';
+import { LoadingScreen } from '../LoadingScreen';
+import { Header, Main } from './Commons';
+
+export const Setting = async ({ children, params }: IPageLayout) => {
+  const { locale } = await params;
+  const dictionary = await getTranslations({
+    locale,
+    namespace: 'UI',
+  });
+
+  return (
+    <Stack height="auto" minHeight="dvh" gap="none">
+      <Header dictionary={dictionary} />
+      <Main>
+        {children}
+      </Main>
+      <Footer dictionary={dictionary} />
+      <LoadingScreen />
+    </Stack>
+  );
+};
