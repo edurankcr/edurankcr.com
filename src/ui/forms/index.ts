7ae3293e347717dfbffff3dc9eecb3b314c0cfc8
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-06 22:44:22 -0600

    feat(Index): export additional components for improved modularity

---
+++ src/ui/forms/index.ts
@@ -0,0 +1,6 @@
+export * from './AvatarForm';
+export * from './BiographyForm';
+export * from './BirthdateForm';
+export * from './EmailForm';
+export * from './NameForm';
+export * from './UsernameForm';
