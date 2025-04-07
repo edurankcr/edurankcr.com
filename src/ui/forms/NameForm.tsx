0e66155c6997b195f51bdbf544110cd409852f23
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-06 22:44:46 -0600

    feat(NameForm): add NameForm component for user profile name and last name updates

---
+++ src/ui/forms/NameForm.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import type { z } from 'zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormSubmit,
+  Group,
+  Input,
+  Stack,
+} from '@/components';
+import { putUserProfile } from '@/services';
+import { useUserStore } from '@/stores';
+import type { ITranslations, IUser } from '@/types';
+import { NameValidation } from '@/validations';
+
+type LegalNameFormProps = {} & ITranslations & IUser;
+
+export const NameForm = ({ dictionary, User }: LegalNameFormProps) => {
+  const form = useForm<z.infer<typeof NameValidation>>({
+    resolver: zodResolver(NameValidation),
+    reValidateMode: 'onSubmit',
+    shouldFocusError: true,
+    defaultValues: {
+      Name: User?.name || '',
+      LastName: User?.lastName || '',
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof NameValidation>) {
+    const { Name, LastName } = values;
+    const setUser = useUserStore.getState().setUser;
+
+    if (!User) {
+      toast.error(dictionary('Errors.Auth.LoginRequired'));
+      return;
+    }
+
+    try {
+      await putUserProfile({ Name, LastName });
+      setUser({ ...User, name: Name, lastName: LastName });
+      toast.success(dictionary('Paragraph.profile_data_updated'));
+    } catch (error: any) {
+      console.error('Error updating user data:', error);
+      toast.error(dictionary('Errors.General.500'));
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <Stack asChild>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <Group flexWrap="nowrap" overflow="visible">
+            <FormField
+              control={form.control}
+              name="Name"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      placeholder={dictionary('Input.Name.placeholder')}
+                      autoComplete="name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="LastName"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      placeholder={dictionary('Input.LastName.placeholder')}
+                      autoComplete="family-name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </Group>
+          <FormSubmit height="md" className="md:w-fit">
+            {dictionary('Button.save')}
+          </FormSubmit>
+        </form>
+      </Stack>
+    </Form>
+  );
+};
