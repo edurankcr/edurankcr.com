f2e48038d7128b87deb8be4bd359e2c361c7d613
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-12 16:39:39 -0600

    fix(ReactQueryProvider): update environment variable access for development mode

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { Env } from '@/services/config/env';
 import type { IChildren } from '@/types';
 
 const queryClient = new QueryClient({
@@ -21,7 +22,7 @@ export function ReactQueryProvider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+      {Env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 }
